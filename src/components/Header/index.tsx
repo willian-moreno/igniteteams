022a4ee3dabae7c82dea72e6dd9e25b55349b180
Willian Alves Moreno <willianam@uninove.br>
2025-02-25 19:15:40 -0300

    feat: Biblioteca de icones e botão de voltar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,24 @@
 import logoImg from '@assets/logo.png'
-import { Container, Logo } from './styles'
 
-export function Header() {
+import { BackButton, BackIcon, Container, Logo } from './styles'
+
+type Props = {
+  showBackButton?: boolean
+  onBackButtonPress?: () => void
+}
+
+export function Header({ showBackButton = false, onBackButtonPress = () => {} }: Props) {
+  function handleOnBackButtonPress() {
+    onBackButtonPress()
+  }
+
   return (
     <Container>
+      {showBackButton && (
+        <BackButton onPress={handleOnBackButtonPress}>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={logoImg} />
     </Container>
   )
