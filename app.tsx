5c021796d981bccd47e89f0362d3f55ae4848794
Willian Alves Moreno <willianam@uninove.br>
2025-02-25 15:46:22 -0300

    feat: Definindo o tema da aplicação

--- app.tsx
+++ app.tsx
@@ -1,11 +1,15 @@
 import { Groups } from '@screens/groups'
+import { theme } from '@themes/index'
 import { StatusBar } from 'react-native'
+import { ThemeProvider } from 'styled-components'
 
 export function App() {
   return (
     <>
-      <StatusBar barStyle={'light-content'} backgroundColor="transparent" translucent />
-      <Groups></Groups>
+      <ThemeProvider theme={theme}>
+        <StatusBar barStyle={'light-content'} backgroundColor="transparent" translucent />
+        <Groups></Groups>
+      </ThemeProvider>
     </>
   )
 }
