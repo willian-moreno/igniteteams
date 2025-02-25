0cb01b9b281e32807f0ebc148a0756148df7556e
Willian Alves Moreno <willianam@uninove.br>
2025-02-25 17:47:49 -0300

    feat: Adicionando fontes personalizadas

--- app.tsx
+++ app.tsx
@@ -1,13 +1,16 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
 import { Groups } from '@screens/groups'
 import { defaultTheme } from '@themes/index'
-import { StatusBar } from 'react-native'
+import { ActivityIndicator, StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components'
 
 export function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <StatusBar barStyle={'light-content'} backgroundColor="transparent" translucent />
-      <Groups></Groups>
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   )
 }
