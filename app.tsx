8a15e92be5d57848691f9970e7b37eb761e1962b
Willian Alves Moreno <willian_moreno@outlook.com>
2025-02-26 19:25:23 -0300

    feat: Criando estrutura de navegação

--- app.tsx
+++ app.tsx
@@ -1,6 +1,6 @@
 import { Loading } from '@components/loading'
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
-import { Players } from '@screens/players'
+import { Routes } from '@routes/index'
 import { defaultTheme } from '@themes/index'
 import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components'
@@ -11,7 +11,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <StatusBar barStyle={'light-content'} backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   )
 }
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Groups } from '@screens/groups'
+import { NewGroup } from '@screens/new-group'
+import { Players } from '@screens/players'
+
+const { Navigator, Screen } = createNativeStackNavigator()
+
+export function AppRoutes() {
+  return (
+    <Navigator initialRouteName="groups" screenOptions={{ headerShown: false }}>
+      <Screen name="groups" component={Groups} />
+      <Screen name="new-group" component={NewGroup} />
+      <Screen name="players" component={Players} />
+    </Navigator>
+  )
+}
