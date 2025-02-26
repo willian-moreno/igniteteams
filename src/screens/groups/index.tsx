109a44577a37a8735085ae01803c30d8b667d84b
Willian Alves Moreno <willian_moreno@outlook.com>
2025-02-26 11:32:19 -0300

    feat: Listando grupos

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,14 +1,34 @@
 import { GroupCard } from '@components/group-card'
 import { Header } from '@components/header'
 import { Highlight } from '@components/highlight'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+import uuid from 'react-native-uuid'
 import { Container } from './styles'
 
+type Group = {
+  id: string
+  title: string
+}
+
 export function Groups() {
+  const [groups, setGroups] = useState<Group[]>([
+    {
+      id: uuid.v4(),
+      title: 'Rocketseat One React Native',
+    },
+  ])
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Nome da turma" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GroupCard title={item.title} />}
+        showsVerticalScrollIndicator={false}
+      />
     </Container>
   )
 }
