b7b0c2e987b70b7731d3c770342a30e21cca8a7b
Willian Alves Moreno <willian_moreno@outlook.com>
2025-02-26 17:09:23 -0300

    feat: Listando os filtros

--- src/screens/players/index.tsx
+++ src/screens/players/index.tsx
@@ -1,17 +1,40 @@
 import { Button } from '@components/button'
 import { ButtonIcon } from '@components/button-icon'
+import { Filter } from '@components/filter'
 import { Header } from '@components/header'
 import { Highlight } from '@components/highlight'
 import { Input } from '@components/input'
+import { useMemo, useState } from 'react'
+import { FlatList } from 'react-native'
+import uuid from 'react-native-uuid'
 import { useTheme } from 'styled-components'
-import { Container, InputContainer } from './styles'
+import { Container, HeaderList, InputContainer, NumberOfPlayers } from './styles'
 
 type Props = {
   classId: string
 }
 
+type Player = {
+  id: string
+  name: string
+}
+
+type Team = {
+  id: string
+  title: string
+  players: Player[]
+}
+
 export function Players({}: Props) {
   const { colors } = useTheme()
+  const [teams, setTeams] = useState<Team[]>([
+    { id: 'team-a', title: 'Time A', players: [{ id: uuid.v4(), name: 'Will' }] },
+    { id: 'team-b', title: 'Time B', players: [] },
+  ])
+  const [activeTeamId, setActiveTeamId] = useState('team-a')
+  const numberOfActiveTeamPlayers = useMemo(() => {
+    return teams.find((team) => team.id === activeTeamId)?.players.length
+  }, [teams, activeTeamId])
 
   return (
     <Container>
@@ -25,6 +48,21 @@ export function Players({}: Props) {
         />
         <ButtonIcon icon="add" />
       </InputContainer>
+      <HeaderList>
+        <FlatList
+          data={teams}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Filter
+              title={item.title}
+              isActive={item.id === activeTeamId}
+              onPress={() => setActiveTeamId(item.id)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>{numberOfActiveTeamPlayers}</NumberOfPlayers>
+      </HeaderList>
       <Button title="Remover turma" variant="secondary" style={{ marginTop: 20 }} />
     </Container>
   )
--- src/screens/players/styles.ts
+++ src/screens/players/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components/native'
+import styled, { css } from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
@@ -17,4 +17,20 @@ export const InputContainer = styled.View`
   border-radius: 6px;
 
   background-color: ${({ theme }) => theme.colors.gray_700};
+`
+
+export const HeaderList = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  width: 100%;
+  margin: 32px 0 12px;
+`
+
+export const NumberOfPlayers = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray_200};
+    font-family: ${theme.font_family.bold};
+    font-size: ${theme.font_size.sm}px;
+  `};
 `
