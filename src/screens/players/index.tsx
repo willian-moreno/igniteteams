5e2195c470d8b0eab13cd8fab0b7ea4055f954b3
Willian Alves Moreno <willian_moreno@outlook.com>
2025-02-26 17:57:38 -0300

    feat: Adicionando e removendo jogadores

--- src/screens/players/index.tsx
+++ src/screens/players/index.tsx
@@ -4,6 +4,7 @@ import { Filter } from '@components/filter'
 import { Header } from '@components/header'
 import { Highlight } from '@components/highlight'
 import { Input } from '@components/input'
+import { PlayerCard } from '@components/player-card'
 import { useMemo, useState } from 'react'
 import { FlatList } from 'react-native'
 import uuid from 'react-native-uuid'
@@ -27,14 +28,50 @@ type Team = {
 
 export function Players({}: Props) {
   const { colors } = useTheme()
+  const [playerName, setPlayerName] = useState('')
   const [teams, setTeams] = useState<Team[]>([
     { id: 'team-a', title: 'Time A', players: [{ id: uuid.v4(), name: 'Will' }] },
     { id: 'team-b', title: 'Time B', players: [] },
   ])
   const [activeTeamId, setActiveTeamId] = useState('team-a')
-  const numberOfActiveTeamPlayers = useMemo(() => {
-    return teams.find((team) => team.id === activeTeamId)?.players.length
+  const activeTeamPlayers = useMemo(() => {
+    return teams.find((team) => team.id === activeTeamId)?.players
   }, [teams, activeTeamId])
+  const numberOfActiveTeamPlayers = useMemo(() => activeTeamPlayers?.length, [teams, activeTeamId])
+
+  function handleAddPlayerInActiveTeam() {
+    setTeams((state) => {
+      const activeTeamIndex = state.findIndex((team) => team.id === activeTeamId)
+
+      const playerAlreadyExists =
+        state[activeTeamIndex].players.findIndex((player) => player.name === playerName) !== -1
+
+      if (playerAlreadyExists) {
+        return state
+      }
+
+      state[activeTeamIndex].players.push({
+        id: uuid.v4(),
+        name: playerName,
+      })
+
+      return [...state]
+    })
+
+    setPlayerName('')
+  }
+
+  function handleRemovePlayerFromActiveTeam(playerId: string) {
+    setTeams((state) => {
+      const activeTeamIndex = state.findIndex((team) => team.id === activeTeamId)
+
+      state[activeTeamIndex].players = state[activeTeamIndex].players.filter(
+        (player) => player.id !== playerId,
+      )
+
+      return [...state]
+    })
+  }
 
   return (
     <Container>
@@ -42,11 +79,13 @@ export function Players({}: Props) {
       <Highlight title="Nome da turma" subtitle="adicione a galera e separe os times" />
       <InputContainer>
         <Input
+          value={playerName}
           placeholder="Nome do participante"
           placeholderTextColor={colors.gray_300}
           autoCorrect={false}
+          onChangeText={(value) => setPlayerName(value)}
         />
-        <ButtonIcon icon="add" />
+        <ButtonIcon icon="add" onPress={handleAddPlayerInActiveTeam} />
       </InputContainer>
       <HeaderList>
         <FlatList
@@ -63,6 +102,17 @@ export function Players({}: Props) {
         />
         <NumberOfPlayers>{numberOfActiveTeamPlayers}</NumberOfPlayers>
       </HeaderList>
+      <FlatList
+        data={activeTeamPlayers}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <PlayerCard
+            name={item.name}
+            onRemovePress={() => handleRemovePlayerFromActiveTeam(item.id)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+      />
       <Button title="Remover turma" variant="secondary" style={{ marginTop: 20 }} />
     </Container>
   )
