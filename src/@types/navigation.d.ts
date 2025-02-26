3928eeebac61e7eaef66e19eefa70604933e43c0
Willian Alves Moreno <willian_moreno@outlook.com>
2025-02-26 19:48:47 -0300

    refactor: Adicionando tipagem rotas

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,14 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      groups: undefined
+      'new-group': undefined
+      players: {
+        group: {
+          id: string
+          title: string
+        }
+      }
+    }
+  }
+}
