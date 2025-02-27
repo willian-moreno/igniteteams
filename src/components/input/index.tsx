03c7a2bf5494d3e0a645c611e2f35120118fc34a
Willian Alves Moreno <willian_moreno@outlook.com>
2025-02-27 18:13:35 -0300

    feat: Criando prop opcional de input ref

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,11 +1,14 @@
-import { TextInputProps } from 'react-native'
+import React, { RefObject } from 'react'
+import { TextInput, TextInputProps } from 'react-native'
 import { useTheme } from 'styled-components'
 import { Container } from './styles'
 
-type Props = {} & TextInputProps
+type Props = {
+  inputRef?: RefObject<TextInput>
+} & TextInputProps
 
-export function Input({ ...props }: Props) {
+export function Input({ inputRef, ...props }: Props) {
   const { colors } = useTheme()
 
-  return <Container placeholderTextColor={colors.gray_300} {...props}></Container>
+  return <Container ref={inputRef} placeholderTextColor={colors.gray_300} {...props}></Container>
 }
